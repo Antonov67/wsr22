edeb3283b0bb8b22f96ff21d7e95c3f45d479a2e
Dmitriy Antonov <79038945169@yandex.ru>
2022-01-12 15:29:49 +0300

    SignInScreen_design

--- app/src/main/java/com/example/wsr22/SignInActivity.java
+++ app/src/main/java/com/example/wsr22/SignInActivity.java
@@ -1,25 +1,24 @@
 package com.example.wsr22;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
         getSupportActionBar().hide();
-        Button button = findViewById(R.id.button_sign_up_exit);
+        Button button = findViewById(R.id.button_sign_in);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               startActivity(new Intent(SignUpActivity.this,SignInActivity.class));
+                startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
             }
         });
     }
