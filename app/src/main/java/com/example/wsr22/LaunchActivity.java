e3ad0e306e017177cfa5cadf004a90c6ebd1fa37
Dmitriy Antonov <79038945169@yandex.ru>
2022-01-12 12:30:46 +0300

    Launch&SplashScreen

---
+++ app/src/main/java/com/example/wsr22/LaunchActivity.java
@@ -0,0 +1,31 @@
+package com.example.wsr22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_main);
+        getSupportActionBar().hide();
+        ImageView imageView = findViewById(R.id.imageView);
+        //анимация
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.combination);
+        imageView.startAnimation(animation);
+        //через 6 секунд открываем основное окно
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+            }
+        }, 6000);
+    }
+}
